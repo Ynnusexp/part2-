2c95c429372f3d810248bf1f5aa992ca1a3c895c
Sophia <sophia.tsau@gmail.com>
2023-07-27 15:40:13 -0700

    completed problems 5-7

--- problems/06-dynamic-interval-count.js
+++ problems/06-dynamic-interval-count.js
@@ -1,32 +1,50 @@
 /***********************************************************************
 Write a function, `dynamicIntervalCount`, that accepts a callback, a delay
-in milliseconds, and an optional amount as arguments. The function should 
-set an interval with the given callback and delay. If an amount argument 
-is passed, the interval should be cleared after the callback has been 
+in milliseconds, and an optional amount as arguments. The function should
+set an interval with the given callback and delay. If an amount argument
+is passed, the interval should be cleared after the callback has been
 called 'amount' number of times. If an amount argument is not passed,
 the interval should not be cleared and `dynamicIntervalCount` should instead
 return the Timeout object for the interval.
 
-In addition to Mocha, we recommend that you test your code manually using 
+In addition to Mocha, we recommend that you test your code manually using
 node with the examples below.
-
-Examples:
-
-dynamicIntervalCount(function() {
-    console.log('hi');
-}, 500, 3); // prints 'hi' at 500ms intervals a total of 3 times
-
-
-const timeoutObject = dynamicIntervalCount(function() {
-    console.log('hi');
-}, 500); // prints 'hi' at 500ms intervals indefinitely
-
-console.log(timeoutObject); // Timeout { ... }
 ***********************************************************************/
-
+//INPUT: callback, delay (ms), optional amount
+/*OUTPUT:
+  setInterval(cb, delay)
+  if amount, clear after cb is called amount number of times
+  if no amount, return timeout object for interval
+    never timeout, keep printing
+    amount === undefined
+*/
+//BASE: amount === 0      -> clearInterval(interval obj)
+//RECURSE: amount > 0     -> dynamicIntervalCount()
+//STEP: amount--
 function dynamicIntervalCount(cb, delay, amount) {
-  // Your code here
+  let intervalObj = setInterval(() => {
+    cb();
+    amount--; //STEP
+    //BASE
+    if (amount === 0) {
+      clearInterval(intervalObj);
+    }
+  },
+  delay);
+  if (amount === undefined) return intervalObj;
 }
+
+debugger;
+// dynamicIntervalCount(function() {
+//   console.log('hi');
+// }, 500, 3); // prints 'hi' at 500ms intervals a total of 3 times
+
+
+// const timeoutObject = dynamicIntervalCount(function() {
+//   console.log('hi');
+// }, 500); // prints 'hi' at 500ms intervals indefinitely
+
+// console.log(timeoutObject); // Timeout { ... }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
